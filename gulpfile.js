32ba60fc3f2dbf955884071bbb76ae0b517389f1
Nick Lemmon <nicklemmonbbt@gmail.com>
2016-09-18 15:42:23 -0400

    General - initializing repo, basic gulpfile complete with some project structure
    Signed-off-by: Nick Lemmon <nicklemmonbbt@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+'use strict';
+
+//////////////////////////
+//== Requiring things ==//
+//////////////////////////
+
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var globSass = require('gulp-sass-glob');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var concatJS = require('gulp-concat');
+
+//////////////////////////
+//== Global variables ==//
+//////////////////////////
+
+// Base paths
+var base = {
+  src: './src/',
+  dist: './_dist/'
+}
+
+// Sub paths
+var path = {
+  styles: 'styles/',
+  markup: 'markup/',
+  js: 'js/'
+}
+
+
+///////////////
+//== Tasks ==//
+///////////////
+
+// Delete the output directory
+gulp.task('clean', function() {
+  return gulp.src(base.dist) // delete the output dir
+    .pipe(clean());
+})
+
+// Build some pages and such
+gulp.task('markup', function() {
+  var YOUR_LOCALS = {}; // TODO - figure this -ish out - probably an alternative to Jekyll config file?
+
+  return gulp.src([
+    base.src + path.markup + '**/!(_)*.pug' // all pug files are compiled, except those beginning with an '_'
+  ])
+    .pipe(pug({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest(base.dist))
+});
+
+// Glob stuff and spit out some styles
+gulp.task('styles', function() {
+  return gulp.src([
+    !base.src + path.styles + 'style.scss', // skip the main file
+    base.src + path.styles + '**/*.scss'
+  ])
+    .pipe(globSass())
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest(base.dist + path.styles))
+})
+
+// Concat and uglify the JavaScript
+gulp.task('scripts', function() {
+  return gulp.src(base.src + path.js + '**/*.js')
+    .pipe(concatJS('concat.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(base.dist + path.js))
+})
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  gulp.run('markup');
+  gulp.run('styles');
+  gulp.run('scripts');
+});
+
+// Watch task
+gulp.task('watch', ['sass:watch'])
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Readme for nicklemmon.com v3
+This project is for my portfolio. I'll be trying to use some new tools, including Webpack for the first time.
+
+## Getting started
+First run `npm install`, then `npm run build` to build the site.
+
+---
+
+## Project Structure
+Sass is structured using ITCSS as described by https://www.xfive.co/blog/itcss-scalable-maintainable-css-architecture/ and https://medium.com/@jordankoschei/how-i-shrank-my-css-by-84kb-by-refactoring-with-itcss-2e8dafee123a#.gpc6n81a2.
+
+The first two directories do not output any CSS.
+
+## Webpack
+I'm looking for a way to integrate Webpack, however, initially using Gulp - may use both though I need to figure out how this sort of combination can function.
