a63d1ae8bd21e6f13ca26e05b3266d10fbe92458
Nick Lemmon <nicklemmonbbt@gmail.com>
2016-11-06 09:22:44 -0500

    Modals now functioning
    Signed-off-by: Nick Lemmon <nicklemmonbbt@gmail.com>

--- src/js/3-modules/Portfolio.Global.Modal.js
+++ src/js/3-modules/Portfolio.Global.Modal.js
@@ -1,16 +1,40 @@
 Portfolio.Global.Modal = {
+  init: function() {
+    this.cacheDom();
+    this.bindEvents();
+  },
   cacheDom: function() {
+    this.$body = $('body');
+    this.$modal = $('.js-modal');
+    this.$overlay = $('.js-modalOverlay');
     this.$openTrigger = $('.js-modalOpen');
     this.$closeTrigger = $('.js-modalClose');
   },
-  openModal: function() {
-    console.log('test');
+  openModal: function($thisElem) {
+    var thisHref = $thisElem.attr('href');
+    var $targetModal = $("[data-target='" + thisHref + "']");
+    var $parentOverlay = $targetModal.closest('.modal__overlay');
+
+    $targetModal.removeClass('is-hidden').addClass('is-visible');
+    $parentOverlay.removeClass('is-hidden').addClass('is-visible');
+    this.$body.addClass('u-overflow--hidden');
   },
   closeModal: function() {
-    console.log('test');
+    this.$modal.removeClass('is-visible').addClass('is-hidden');
+    this.$overlay.removeClass('is-visible').addClass('is-hidden');
+    this.$body.removeClass('u-overflow--hidden');
   },
-  init: function() {
-    this.cacheDom();
-    console.log('firing');
+  bindEvents: function() {
+    this.$openTrigger.on('click keypress', function(e) {
+      e.preventDefault();
+
+      if (e.which === 13 || e.type === 'click') {
+        Portfolio.Global.Modal.openModal($(this));
+      }
+    });
+
+    this.$overlay.on('click', function() {
+      Portfolio.Global.Modal.closeModal();
+    });
   }
 }
