b5ad94c2be56bff0391831054e0d49b707194540
Nick Lemmon <nicklemmonbbt@gmail.com>
2016-11-06 21:28:12 -0500

    Adding rellax parallax plugin
    Signed-off-by: Nick Lemmon <nicklemmonbbt@gmail.com>

--- src/js/4-init/init.js
+++ src/js/4-init/init.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+  //== webP polyfill
   webpPolyfill.evaluate($('img'));
 
+  //== modals/lightboxes
   if ( $('.js-modalOpen').length ) {
     Portfolio.Global.Modal.init();
   }
+
+  //== rellax parallax plugin
+  var rellax = new Rellax('.js-parallax', {speed: -1})
 
 });
