1da3aadaea641f16eb5cbd2e4f8e138b46d5ef0e
Nick Lemmon <nicklemmonbbt@gmail.com>
2016-11-30 19:15:36 -0500

    Initializing menu button function
    Signed-off-by: Nick Lemmon <nicklemmonbbt@gmail.com>

--- src/js/4-init/init.js
+++ src/js/4-init/init.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   //== modals/lightboxes
-  if ( $('.js-modalOpen').length ) {
+  if ($('.js-modalOpen').length) {
     Portfolio.Global.Modal.init();
   };
 
   //== skill cards
-  if ( $('.js-skillCard').length ) {
+  if ($('.js-skillCard').length) {
     Portfolio.Global.SkillCard.init();
+  };
+
+  //== nav menu
+  if ($('.js-menu').length) {
+    Portfolio.Global.MenuBtn.init();
   };
 });
