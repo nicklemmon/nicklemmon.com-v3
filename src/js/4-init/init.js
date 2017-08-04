f093c4f8d26887509a3c16a1f36a3dabb5972b1f
Nick Lemmon <nicklemmonbbt@gmail.com>
2017-08-03 20:16:38 -0400

    Adjusting spacing on init file
    Signed-off-by: Nick Lemmon <nicklemmonbbt@gmail.com>

--- src/js/4-init/init.js
+++ src/js/4-init/init.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener( 'DOMContentLoaded', function( event ) {
   //== modals/lightboxes
-  if ($('.js-modalOpen').length) {
+  if ( $( '.js-modalOpen' ).length ) {
     Portfolio.Modal.init();
   };
 
   //== skill cards
-  if ($('.js-skillCard').length) {
+  if ( $( '.js-skillCard' ).length ) {
     Portfolio.SkillCard.init();
   };
 
   //== nav menu
-  if ($('.js-menu').length) {
+  if ( $( '.js-menu' ).length ) {
     Portfolio.MenuBtn.init();
   };
 
   //== ajax object
-  if($('.js-ajaxTrigger').length) {
+  if( $( '.js-ajaxTrigger' ).length ) {
     Portfolio.Ajax.init();
   };
 });
